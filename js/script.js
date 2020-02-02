257dd4c34dcc2d7679b04e48a7bad283675354a3
viktr 666 <49025707+viktr1432@users.noreply.github.com>
2020-02-02 20:38:29 +0300

    card-product ready!!!

--- js/script.js
+++ js/script.js
@@ -9,46 +9,46 @@ jQuery(document).ready(function () {
 
 // modal
 
-function modal() {
+// function modal() {
 
-  const openModalButtons = document.querySelectorAll('[data-modal-target]');
-  const closeModalButtons = document.querySelectorAll('[data-close-button]');
-  const overlay = document.getElementById('overlay');
+//   const openModalButtons = document.querySelectorAll('[data-modal-target]');
+//   const closeModalButtons = document.querySelectorAll('[data-close-button]');
+//   const overlay = document.getElementById('overlay');
 
-  openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const modal = document.querySelector(button.dataset.modalTarget)
-      openModal(modal)
-    })
-  })
+//   openModalButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//       const modal = document.querySelector(button.dataset.modalTarget)
+//       openModal(modal)
+//     })
+//   })
 
-  overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.modal.active')
-    modals.forEach(modal => {
-      closeModal(modal)
-    })
-  })
+//   overlay.addEventListener('click', () => {
+//     const modals = document.querySelectorAll('.modal.active')
+//     modals.forEach(modal => {
+//       closeModal(modal)
+//     })
+//   })
 
-  closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const modal = button.closest('.remodal')
-      closeModal(modal)
-    })
-  })
+//   closeModalButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//       const modal = button.closest('.remodal')
+//       closeModal(modal)
+//     })
+//   })
 
-  function openModal(modal) {
-    if (modal == null) return
-    modal.classList.add('active')
-    overlay.classList.add('active')
-  }
+//   function openModal(modal) {
+//     if (modal == null) return
+//     modal.classList.add('active')
+//     overlay.classList.add('active')
+//   }
 
-  function closeModal(modal) {
-    if (modal == null) return
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
-  }
-}
-modal();
+//   function closeModal(modal) {
+//     if (modal == null) return
+//     modal.classList.remove('active')
+//     overlay.classList.remove('active')
+//   }
+// }
+// modal();
 
 // paralax-effect
 
